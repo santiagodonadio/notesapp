483459cc9180a5393acc155f33bc0d4e31bf77be
santiagodonadio <sdonadio@worcester.edu>
2025-04-22 17:17:41 -0400

    connecting to screen

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
+// Fetching the URL and Key from environment variables
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+// Initialize Supabase client
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
